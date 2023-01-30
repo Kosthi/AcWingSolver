4e179176cf35f973858847b2c83440c019962b3b
Koschei <nitianzero@gmail.com>
2023-01-30 20:51:06 +0800

    chore: partial_sum_Template--Day5

---
+++ 796. 子矩阵的和.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1e4;
+
+int n, m, q;
+int s[N][N];
+
+
+int main() {
+    scanf("%d %d %d", &n, &m, &q);
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            scanf("%d", &s[i][j]);
+            s[i][j] += s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1];
+        }
+    }
+    
+    while (q--) {
+        int x1, y1, x2, y2;
+        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+        printf("%d\n", s[x2][y2] - s[x1 - 1][y2] - s[x2][y1 - 1] + s[x1 - 1][y1 - 1]);
+    }
+}
