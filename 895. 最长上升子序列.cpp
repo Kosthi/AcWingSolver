b48dc29991d17cd1b166a06c805d73fca8210345
Koschei <nitianzero@gmail.com>
2023-01-31 21:35:04 +0800

    chore: dynamic_programming_Template--Day6

---
+++ 895. 最长上升子序列.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1010;
+
+int n;
+int a[N], f[N];
+
+
+int main() {
+    cin >> n;
+    for (int r = 1; r <= n; r++) {
+        scanf("%d", &a[r]);
+        for (int l = 1; l < r; ++l) {
+            if (a[l] < a[r]) {
+                f[r] = max(f[r], f[l] + 1);
+                f[0] = max(f[0], f[r]);
+            }
+        }
+    }
+    cout << f[0] + 1 << endl;
+}
