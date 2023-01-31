05013fd88f88186964c9f767226e7396bad9bcd0
Koschei <nitianzero@gmail.com>
2023-01-31 21:43:08 +0800

    perf: reduce recyle count

--- 895. 最长上升子序列.cpp
+++ 895. 最长上升子序列.cpp
@@ -13,14 +13,14 @@ int a[N], f[N];
 
 int main() {
     cin >> n;
-    for (int r = 1; r <= n; r++) {
+    for (int r = 2; r <= n; r++) {
         scanf("%d", &a[r]);
         for (int l = 1; l < r; ++l) {
             if (a[l] < a[r]) {
                 f[r] = max(f[r], f[l] + 1);
-                f[0] = max(f[0], f[r]);
             }
         }
+	f[0] = max(f[0], f[r]);
     }
     cout << f[0] + 1 << endl;
 }
