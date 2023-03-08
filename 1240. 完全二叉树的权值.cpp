1dde6b20e2db23ec708f431c20085f73d3da3f70
Koschei <nitianzero@gmail.com>
2023-03-08 23:53:23 +0800

    feat: partial_sum_slove

--- 1240. 完全二叉树的权值.cpp
+++ 1240. 完全二叉树的权值.cpp
@@ -25,3 +25,37 @@ int main() {
     }
     printf("%d", depth);
 }
+
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+const int N = 100010;
+
+typedef long long LL;
+
+int n;
+LL a[N];
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+        scanf("%lld", &a[i]);
+        a[i] += a[i - 1];
+    }
+    
+    int depth = 0;
+    LL sum = INT64_MIN;
+    for (int i = 1, d = 1; i <= n; i += (1 << d - 1), d++) {
+        LL s;
+        if (i + (1 << d - 1) - 1 <= n)
+            s = a[i + (1 << d - 1) - 1] - a[i - 1];
+        else
+            s = a[n] - a[i - 1];
+        if (s > sum) {
+            sum = s;
+            depth = d;
+        }
+    }
+    printf("%d", depth);
+}
