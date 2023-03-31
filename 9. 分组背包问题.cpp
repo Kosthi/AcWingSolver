0cdc7995a35894383f18cdb56e050929411f0ba2
Koschei <nitianzero@gmail.com>
2023-03-31 17:10:53 +0800

    feat: memory_optimize_group_bags_dp--Day37
    \

--- 9. 分组背包问题.cpp
+++ 9. 分组背包问题.cpp
@@ -20,3 +20,30 @@ int main() {
     }
     cout << dp[V] << endl;
 }
+// Memory Optimize
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int N = 110;
+
+int n, V, k;
+int v, w, nums;
+int dp[N], last[N];
+
+int main() {
+    cin >> n >> V;
+    for (int i = 1; i <= n; ++i) {
+        cin >> nums;
+        for (int j = 1; j <= nums; ++j) {
+            cin >> v >> w;
+            for (int j = V; j >= v; --j)
+                dp[j] = max(dp[j], last[j - v] + w);
+        }
+        // 使用last数组保留里上一个dp数组，等效于dp[i - 1][j - v].
+        // 故可以在内部遍历体积。
+        memcpy(last, dp, sizeof dp);
+    }
+    cout << dp[V] << endl;
+}
