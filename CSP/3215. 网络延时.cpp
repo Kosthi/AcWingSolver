e1bed2adf1e4fae7983dc1ad5dd6d9aed1c95b4b
Koschei <nitianzero@gmail.com>
2023-05-16 21:41:51 +0800

    chore: tree_diameter_bfs--Day61

---
+++ CSP/3215. 网络延时.cpp
@@ -0,0 +1,118 @@
+// STL Ver
+#include <cstdio>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+
+const int N = 20010, M = 2 * N;
+
+int n, m;
+int h[N], e[M], ne[M], idx;
+int vis[M];
+int node;
+queue<int> q;
+
+void add(int a, int b) {
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+int bfs(int p) {
+    memset(vis, 0, sizeof vis);
+    q.push(p);
+    vis[p] = 1;
+    int dist = 0;
+    while (q.size()) {
+        int s = q.size();
+        while (s--) {
+            int t = q.front(); q.pop();
+            node = t;
+            for (int i = h[t]; ~i; i = ne[i]) {
+                int j = e[i];
+                if (!vis[j]) {
+                    q.push(j);
+                    vis[j] = 1;
+                }
+            }
+        }
+        dist++;
+    }
+    return dist - 1;
+}
+
+int main() {
+    memset(h, -1, sizeof h);
+    scanf("%d %d", &n, &m);
+    int f;
+    for (int i = 2; i <= n; ++i) {
+        scanf("%d", &f);
+        add(f, i), add(i, f);
+    }
+    for (int i = n + 1; i <= n + m; ++i) {
+        scanf("%d", &f);
+        add(f, i), add(i, f);
+    }
+    bfs(1);
+    printf("%d", bfs(node));
+}
+
+// simulate Ver
+#include <cstdio>
+#include <cstring>
+#include <queue>
+
+using namespace std;
+
+const int N = 20010, M = 2 * N;
+
+int n, m;
+int h[N], e[M], ne[M], idx;
+int vis[M];
+int node;
+int q[N];
+
+void add(int a, int b) {
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+int bfs(int p) {
+    int hh = 0, tt = 0;
+    memset(vis, 0, sizeof vis);
+    q[hh] = p;
+    vis[p] = 1;
+    int dist = 0, s = 1, a = 1;
+    while (hh <= tt) {
+        s = a;
+        a = 0;
+        while (s--) {
+            int t = q[hh++];
+            node = t;
+            for (int i = h[t]; ~i; i = ne[i]) {
+                int j = e[i];
+                if (!vis[j]) {
+                    a++;
+                    q[++tt] = j;
+                    vis[j] = 1;
+                }
+            }
+        }
+        dist++;
+    }
+    return dist - 1;
+}
+
+int main() {
+    memset(h, -1, sizeof h);
+    scanf("%d %d", &n, &m);
+    int f;
+    for (int i = 2; i <= n; ++i) {
+        scanf("%d", &f);
+        add(f, i), add(i, f);
+    }
+    for (int i = n + 1; i <= n + m; ++i) {
+        scanf("%d", &f);
+        add(f, i), add(i, f);
+    }
+    bfs(1);
+    printf("%d", bfs(node));
+}
