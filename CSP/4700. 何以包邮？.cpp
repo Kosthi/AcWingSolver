bab52300e76ce4180cfa8e2329147602dcb93dab
Koschei <nitianzero@gmail.com>
2023-05-17 16:38:09 +0800

    chore: dp_bags_dfs_cut--Day61

--- CSP/4700. 何以包邮？.cpp
+++ CSP/4700. 何以包邮？.cpp
@@ -1,3 +1,4 @@
+// 背包dp
 #include <cstdio>
 #include <iostream>
 
@@ -18,4 +19,39 @@ int main() {
         }
     }
     printf("%d", s - f[s - m]);
+}
+
+// 剪枝DFS
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+
+const int N = 40;
+
+int n, m, v[N];
+int ans = 1e8, st[N];
+
+void dfs(int u, int s) {
+    if (s >= ans) return;
+    int p = s;
+    for (int i = u; i <= n; ++i) p += v[i];
+    if (p < m) return;
+    if (u > n) {
+        if (s >= m && ans > s) ans = s;
+        return;
+    }
+    dfs(u + 1, s);
+    dfs(u + 1, s + v[u]);
+}
+
+bool cmp(int a, int b) {
+    return a > b;
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= n; ++i) scanf("%d", &v[i]);
+    std::sort(v + 1, v + n + 1, cmp);
+    dfs(1, 0);
+    printf("%d", ans);
 }
