c355db474d9a2dc563e2e0cbaee776ca048901ec
Koschei <nitianzero@gmail.com>
2023-06-15 21:33:51 +0800

    chore: find_rules--Day76

---
+++ NOIP/459. 螺旋矩阵.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int n, i, j;
+
+int dfs(int n, int i, int j) {
+    if (i == 1) return j;
+    if (j == n) return n + i - 1;
+    if (i == n) return 3 * n - j - 1;
+    if (j == 1) return 4 * n - i - 2;
+    return dfs(n - 2, i - 1, j - 1) + 4 * (n - 1);
+}
+
+int main() {
+    scanf("%d %d %d", &n, &i, &j);
+    printf("%d", dfs(n, i, j));
+}
